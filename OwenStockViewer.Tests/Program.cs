4fdf432b57614b6e69aadfede6c2c536e622d487
Owen Shelton <o_shelton@yahoo.com>
2015-01-14 19:48:29 -0500

    Initial commit of application and structure.

---
+++ OwenStockViewer.Tests/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Owen
+ * Date: 1/14/2015
+ * Time: 7:34 PM
+ */
+using System;
+
+namespace OwenStockViewer.Tests
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
